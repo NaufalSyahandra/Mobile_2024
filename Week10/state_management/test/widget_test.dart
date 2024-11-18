54a812d5c0c39e0a86b279698910f64c5b81b99a
NaufalSyahandra <114036607+NaufalSyahandra@users.noreply.github.com>
2024-11-18 13:01:05 +0700

    week 10 - state management

--- Week10/state_management/test/widget_test.dart
+++ Week10/state_management/test/widget_test.dart
@@ -1,20 +1,19 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:camera/camera.dart';
 
-import 'package:camera_flutter/main.dart';
+import 'package:state_management/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Buat CameraDescription dummy untuk pengujian
-    const camera = CameraDescription(
-      name: '0',
-      lensDirection: CameraLensDirection.back,
-      sensorOrientation: 90,
-    );
-
-    // Bangun aplikasi dengan dummy camera
-    await tester.pumpWidget(const MyApp(camera: camera));
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
--- Week9/camera_flutter/lib/main.dart
+++ Week9/camera_flutter/lib/main.dart
@@ -4,18 +4,22 @@ import 'widget/takepicture_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Dapatkan daftar kamera yang tersedia
   final cameras = await availableCameras();
-
-  // Pilih kamera pertama
   final firstCamera = cameras.first;
+  runApp(MyApp(camera: firstCamera));
+}
 
-  runApp(
-    MaterialApp(
+class MyApp extends StatelessWidget {
+  final CameraDescription? camera;
+
+  const MyApp({Key? key, required this.camera}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       theme: ThemeData.dark(),
-      home: TakePictureScreen(camera: firstCamera),
+      home: TakePictureScreen(camera: camera!),
       debugShowCheckedModeBanner: false,
-    ),
-  );
+    );
+  }
 }
